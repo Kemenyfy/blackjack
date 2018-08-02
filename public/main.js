ae6e82046bae37260a0f4469a6daa739ee125447
Kemenyfy <gustavo.kemenyfy@gmail.com>
2018-08-02 16:51:10 -0400

    Shuffling and dealing plus basic HTML and CSS

--- public/main.js
+++ public/main.js
@@ -1,5 +1,58 @@
-const main = () => {
-  document.querySelector('h1').textContent += '?'
+const suits = ["Hearts", "Spades", "Clubs", "Diamonds"]
+const rank = ['Ace of ', '2 of ', '3 of ', '4 of ', '5 of ', '6 of ', '7 of ', '8 of ', '9 of ', '10 of ', 'Jack of ', 'Queen of ', 'King of ']
+const cardValue = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+const deck = []
+
+const playAgain = () => {
+  deck.splice(0)
+  for (let s = 0; s < suits.length; s++) {
+    for (let r = 0; r < rank.length; r++) {
+      deck.push({
+        suit: suits[s],
+        rank: rank[r],
+        value: cardValue[r]
+      })
+    }
+  }
+
+
+  for (i = 0; i < deck.length; i++) {
+    j = Math.floor(Math.random() * (i + 1))
+    let temp = null
+    temp = deck[i]
+    deck[i] = deck[j]
+    deck[j] = temp
+  }
+  console.log(deck)
+  cardDealt = deck.pop()
+  console.log(cardDealt); 
+  document.querySelector('.dealtCardOne').textContent = cardDealt.rank + cardDealt.suit;
+  cardDealt = deck.pop()
+  console.log(cardDealt); 
+  document.querySelector('.dealtCardTwo').textContent = cardDealt.rank + cardDealt.suit;
+  cardDealt = deck.pop()
+  console.log(cardDealt); 
+  document.querySelector('.dealtCardThree').textContent = cardDealt.rank + cardDealt.suit;
+  cardDealt = deck.pop()
+  console.log(cardDealt); 
+  document.querySelector('.dealtCardFour').textContent = cardDealt.rank + cardDealt.suit;
 }
 
-document.addEventListener('DOMContentLoaded', main)
+const hitPlayer = () => {
+  cardDealt = deck.pop()
+  console.log(cardDealt); 
+  document.querySelector('.dealtCardFive').textContent = cardDealt.rank + cardDealt.suit;
+}
+
+
+
+document.querySelector('.play').addEventListener('click', playAgain)
+document.querySelector('.hit').addEventListener('click', hitPlayer)
+
+// const li = document.createElement('li')
+
+//   li.textContent = buyer.name
+//   // add the class
+//   li.classList.add('raffle-contestant')
+//   bucketList.appendChild(li)
+//   document.querySelector('#nameTextbox').value = ''
---
+++ requirements.md
@@ -0,0 +1,27 @@
+(PEDAC)
+1. Understand the problem
+2. Examples
+3. Data Structure
+4. Understand the Algorithm
+5. Code with Intent
+
+
+### Requirements Explorer Mode
+
+- [x] 52 Card Deck must be shuffled
+    - [x] Cards must have a Suit, Rank and Value
+    - [x] Play Button
+- [x] Two Cards (Revealed) have to be dealt to the human player
+    - [ ] Triggered by the Play Again Button
+- [x] Two Cards (Hidden) have to be dealt to the computer player (i.e. House)
+    - [ ] Triggered by the Play Again Button
+- [ ] Human player must decide to "Hit" or "Stay"
+    - If "Hit" add another card.
+    - If "Stay" cede turn to the house.
+- [ ] If the Human player "Busts" then Game is Over. Human player loses.
+- [ ] The House reveals cards dealt.
+- [ ] The House "Hits" until 17 is achieved or "Bust".
+- [ ] If The House "Busts" then Game is Over. Human player wins.
+- [ ] Compare Cards, whoever is closer to 21 wins.
+
+### Requirements Adventure Mode
